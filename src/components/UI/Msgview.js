29448fa7f028ff4235c66915562d80e992472eb4
akinGC <akarshgc1998@gmail.com>
2022-10-16 15:20:36 +0530

    read messgaes

---
+++ src/components/UI/Msgview.js
@@ -0,0 +1,32 @@
+import ReactQuill from 'react-quill';
+import './Msg.css'
+
+function Msgview(props) {
+    console.log(props)
+    
+    function backbtn(){
+        props.setShowmsg(false)
+    }
+  
+    return ( 
+        <div className='msgv_whole'>
+                <div className='msgv_backbtn'><span onClick={backbtn} className='msgv_bck_btn'>Back</span></div>
+                <span className='msgv_subject'>{props.arr.subject}</span>
+                <div className='msgv_cnt'>
+                    <div className='msgv_ft'>
+                        <span className='msgv_ft_from'>from: <span className='addslant'>{props.arr.from}</span></span>
+                        <span className='msgv_ft_from'>to:<span className='addslant addslantscnd'>{props.arr.name}</span></span>
+                    </div>
+                    <div className='msgv_body'>
+                    <ReactQuill
+   value={props.arr.content}
+   readOnly={true}
+   theme={"bubble"}
+/>
+                    </div>
+                </div>
+        </div>
+     );
+}
+
+export default Msgview;
--- src/components/UI/Wel.js
+++ src/components/UI/Wel.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
+import ReactQuill from 'react-quill';
 import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Msgview from './Msgview';
 import './Wel.css'
 function Wel() {
-    const [selitm,setitm] = useState('inbox')
+    const [showmsg,setShowmsg] = useState(false)
+    const [selitm,setitm] = useState([])
     const username = useSelector(state=>state.auth.mailId)
 
     const [farry,sarry] = useState([])
@@ -12,11 +15,11 @@ function Wel() {
     async function fetchitms(){
         const resp = await fetch(`https://react-2fea7-default-rtdb.asia-southeast1.firebasedatabase.app/mailapp/${username}.json`)
         const data = await resp.json()
-        console.log(username)
+        // console.log(username)
         let ar1= Object.keys(data)
-        console.log(ar1)
+        // console.log(ar1)
         let ar2 = Object.values(data)
-        console.log(ar2)
+        // console.log(ar2)
         let interarr = []
         for (let i = 0; i < ar1.length; i++) {
            let newobj={
@@ -32,7 +35,7 @@ function Wel() {
         }
         interarr.reverse()
         sarry(interarr)
-        console.log(farry)
+        // console.log(farry)
       
     }
     useEffect(()=>{
@@ -42,7 +45,7 @@ function Wel() {
     function btnclk(e){
         // console.log(e.target.name)
         sedelar([...delar,e.target.name])
-        console.log(delar)
+        // console.log(delar)
 
     }
   async function deletemail(){
@@ -60,7 +63,29 @@ function Wel() {
             
         }
         sarry([ar])
-        console.log(farry)
+        // console.log(farry)
+    }
+    function msgSelcted(e){
+        let idz = e.target.parentElement.id
+        // console.log(idz)
+        for (let i = 0; i < farry.length; i++) {
+            
+            if(farry[i].id==idz){
+                console.log(farry[i].name)
+                setitm({
+                    from:farry[i].from,
+                    content:farry[i].content,
+                    subject:farry[i].subject,
+                    id:farry[i].id,
+                    name:farry[i].name
+                })
+            }
+            
+        }
+       
+         
+           setShowmsg(true)  
+        //    console.log(selitm)
     }
     return ( 
         <div className="wel_whole">
@@ -83,17 +108,22 @@ function Wel() {
             </div>
             <div className="wel_rght inbox_itms">
 
-                {   farry.length!=0?
+               {!showmsg &&<div className='wel_itms_view'>
+               {   farry.length!=0?
                     farry.map((itms)=>(
                   
 <div className="wel_rt_itm" id={itms.id}>
                     <input type='checkbox'name={itms.id} onClick={btnclk}></input>
-                    <span className="wel_itm_title">{itms.from}</span>
-                    <span className="wel_itm_subject">{itms.subject}</span>
+                    <span className="wel_itm_title" onClick={msgSelcted}>{itms.from}</span>
+                    <span className="wel_itm_subject wel_itm_title"onClick={msgSelcted}>{itms.subject}</span>
                 </div>
                     )):<div >No mails</div>
                     }
-
+               </div>}
+             
+                    {
+                        showmsg && <Msgview arr = {selitm} setShowmsg={setShowmsg}/>
+                    }
             </div>
 
             
